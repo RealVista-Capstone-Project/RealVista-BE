71597dd95e1bfccfa38221972012d258ec25bc84
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2026-01-01 15:27:46 +0700

    init

---
+++ src/main/java/com/sep/realvista/RealvistaApplication.java
@@ -0,0 +1,13 @@
+package com.sep.realvista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealvistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealvistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sep/realvista/RealvistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep.realvista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealvistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
