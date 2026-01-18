fdafe974c8171b5c9075364881694f17f099602c
doubletake30 <86301855+doubletake30@users.noreply.github.com>
2026-01-18 16:39:07 +0700

    ref: lint

--- src/main/java/com/sep/realvista/infrastructure/config/map/GoogleMapsProperties.java
+++ src/main/java/com/sep/realvista/infrastructure/config/map/GoogleMapsProperties.java
@@ -1,6 +1,5 @@
 package com.sep.realvista.infrastructure.config.map;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Getter;
