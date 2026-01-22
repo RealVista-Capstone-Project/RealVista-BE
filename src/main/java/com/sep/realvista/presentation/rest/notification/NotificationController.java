2cbc4d8a932b2d4d836b1b88a0493b5a36791123
doubletake30 <86301855+doubletake30@users.noreply.github.com>
2026-01-23 00:28:18 +0700

    Fix checkstyle: remove star imports in NotificationController

--- src/main/java/com/sep/realvista/presentation/rest/notification/NotificationController.java
+++ src/main/java/com/sep/realvista/presentation/rest/notification/NotificationController.java
@@ -4,7 +4,10 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
