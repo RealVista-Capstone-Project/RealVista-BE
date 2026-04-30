02531c1214050878d39e39405e7ef9633e6c96b6
nguyen <yugivip277@gmail.com>
2026-04-30 12:25:26 +0700

    feat: api crud dashboard

---
+++ src/main/java/com/sep/realvista/application/dashboard/dto/DashboardStatsResponse.java
@@ -0,0 +1,25 @@
+package com.sep.realvista.application.dashboard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DashboardStatsResponse {
+    private BigDecimal totalRevenue;
+    private BigDecimal totalRevenueTrend;
+    private Long activeListing;
+    private BigDecimal activeListingTrend;
+    private Long totalClosed;
+    private BigDecimal totalClosedTrend;
+    private Long activeLeads;
+    private BigDecimal activeLeadsTrend;
+    private Long onProgress;
+    private Long closedDeals;
+}
